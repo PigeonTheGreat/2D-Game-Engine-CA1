779c4402e3fb599a8bf4441859c5fad43ede9709
PigeonTheGreat <masseygirl@outlook.ie>
2025-10-12 20:41:45 +0100

    Even more Tilemap... But also Player!
    Added more tiles, added the player to the game and began making the player script!!!!

---
+++ CA1 2D Game Engine/Assets/Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+
+    //Declaring Variables
+    public float speed;
+    private Animator animator;
+    private int scrapCount = 0;
+    [SerializeField] private UIManager ui;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
