20c9d3dc683c1f99c2ac7173b4585532bfddc98a
PigeonTheGreat <masseygirl@outlook.ie>
2025-10-22 16:21:48 +0100

    Moving the player and fixing tilemap collider

--- CA1 2D Game Engine/Assets/Assets/Scripts/Player.cs
+++ CA1 2D Game Engine/Assets/Assets/Scripts/Player.cs
@@ -2,31 +2,44 @@ using UnityEngine;
 using System.Collections.Generic;
 using System.Collections;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class Player : MonoBehaviour
 {
 
     //Declaring Variables
     public float speed; //To assign the speed of the player.
     private Animator animator; //To assign animations to the player as they move.
-    private int scrapCount = 0; //To count the scrap the player picks up.
-    [SerializeField] private UIManager ui; //To link the scrapCount to the counter in UI.
+    //private int scrapCount = 0; //To count the scrap the player picks up.
+    private Rigidbody2D rb; //Assigning a rigidbody2d  variable.
+    //[SerializeField] private UIManager ui; //To link the scrapCount to the counter in UI.
+    private Vector2 startPosition;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        //Calling the components.
         animator = GetComponent<Animator>(); //Linking to the animator.
+        rb = GetComponent<Rigidbody2D>(); //Linking to the Rigidbody2D
+        startPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Moving the player
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 position = transform.position;
+        position.x += Time.deltaTime* moveHorizontal * speed;
 
+        position.y += Time.deltaTime * moveVertical * speed;
+        transform.position = position;
+        //updateAnimator(moveHorizontal, moveVertical);
     }
 
     //Calling a method to count the scraps collected.
-    public void CollectScrap()
-    {
-        scrapCount++;
-        ui.SetScore(scrapCount);
-    }
+    //public void CollectScrap()
+    //{
+    //    scrapCount++;
+    //    ui.SetScore(scrapCount);
+    //}
 }
