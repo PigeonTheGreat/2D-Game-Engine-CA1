a9a6150b2f548fa2bc0ce54dab1e72ac340b2cc3
PigeonTheGreat <masseygirl@outlook.ie>
2025-10-28 16:53:51 +0000

    Sprites/Projectile
    Adding the enemy sprites and prefabs. Setting up the projectile script.

---
+++ CA1 2D Game Engine/Assets/Assets/Scripts/Projectile.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+
+    Rigidbody2D rbody;
+
+    void Awake() //Callled when a gameObject is initialized, initializing rigidbody2d.
+    {
+
+        rbody = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    public void Launch(Vector2 direction, float force)
+    {
+
+        rbody.AddForce(direction * force);
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        Destroy(gameObject);
+
+    }
+
+}
